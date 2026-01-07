503fa181c3d4aef9dc72950b4a8e90dd9105639b
Charles Dustin Smith <charlesdsmith19@gmail.com>
2026-01-06 22:04:59 -0500

    Added some changes

---
+++ Components/Models/TaskerItem.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations; 
+
+namespace BlazorTasker.Models;
+public class TaskerItem
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    
+    [Required(ErrorMessage ="Every Task must have a name.")]
+    public string Name { get; set; } = string.Empty;
+
+    public bool IsComplete { get; set; } = false;
+}
